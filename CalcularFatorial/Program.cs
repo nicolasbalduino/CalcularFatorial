263dd23389d5b89482d8d36de4272e02ffe7d089
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 16:26:01 -0300

    Add project files.

---
+++ CalcularFatorial/Program.cs
@@ -0,0 +1,16 @@
+﻿int num, fatorial = 1;
+
+Console.Write("Digite um numero para calcular seu fatorial: ");
+num = int.Parse(Console.ReadLine());
+
+for (int i = num; i > 0; i--)
+{
+    if (i == 1)
+        Console.WriteLine("1! = 1");
+    else
+        Console.WriteLine($"{i}! = {i} x {i - 1}!");
+
+    fatorial *= i;
+}
+
+Console.WriteLine($"\n{num}! = {fatorial}");
