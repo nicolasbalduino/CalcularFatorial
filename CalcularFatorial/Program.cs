ee3812e5f801e1c7c26b84baf3dac812f986ad65
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-06 23:35:44 -0300

    criada a funcao recursiva calcularFatorial

--- CalcularFatorial/Program.cs
+++ CalcularFatorial/Program.cs
@@ -1,16 +1,13 @@
-﻿int num, fatorial = 1;
+﻿int num;
+
+int calcularFatorial(int valor)
+{
+    if (valor == 0)
+        return 1;
+    return valor * (calcularFatorial(valor - 1));
+}
 
 Console.Write("Digite um numero para calcular seu fatorial: ");
 num = int.Parse(Console.ReadLine());
 
-for (int i = num; i > 0; i--)
-{
-    if (i == 1)
-        Console.WriteLine("1! = 1");
-    else
-        Console.WriteLine($"{i}! = {i} x {i - 1}!");
-
-    fatorial *= i;
-}
-
-Console.WriteLine($"\n{num}! = {fatorial}");
+Console.WriteLine($"\n{num}! = {calcularFatorial(num)}");
